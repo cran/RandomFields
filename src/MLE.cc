3e7d0eda861a94bf29844315f049e11eda363007
Martin Schlather <schlather@math.uni-mannheim.de>
2013-10-30 00:00:00 +0000

    version 2.0.66

--- src/MLE.cc
+++ src/MLE.cc
@@ -22,10 +22,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-extern "C" {
 #include <R.h>
 #include <Rdefines.h>
-}
 #include <R_ext/Linpack.h>
 #include <math.h>  
 #include <stdio.h>  
